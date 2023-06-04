e29f6a7d9bb89347fa93f70376009d6df0d4daca
David Volm <david@volminator.com>
2023-06-04 09:19:22 -0500

    Initial version written by GPT4

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	openai "github.com/sashabaranov/go-openai"
+	"gopkg.in/yaml.v2"
+)
+
+// Config represents OpenAI API configuration
+type Config struct {
+	Token string `yaml:"token"`
+}
+
+func loadConfig() (*Config, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadFile(fmt.Sprintf("%s/.config/git-gpt/openai.yaml", home))
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func generateCommitMessage(diff string, config *Config) (string, error) {
+	client := openai.NewClient(config.Token)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Generate a git commit message for:\n" + diff,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	return resp.Choices[0].Message.Content, nil
+}
+
+func main() {
+	autoCommit := flag.Bool("a", false, "Auto commit flag")
+	message := flag.Bool("m", false, "Skip message flag")
+	flag.Parse()
+
+	config, err := loadConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	diffCmd := exec.Command("git", "diff")
+	diffOutput, err := diffCmd.Output()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	msg, err := generateCommitMessage(string(diffOutput), config)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var commitCmd *exec.Cmd
+	if *autoCommit {
+		if *message {
+			commitCmd = exec.Command("git", "commit", "-a", "-m", msg)
+		} else {
+			commitCmd = exec.Command("git", "commit", "-a")
+		}
+		err = commitCmd.Run()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
